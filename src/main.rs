78defbfa3ccc461989115305e3ecc6fc5d3c0c0c
wulawulu <wutuobangbang@gmail.com>
2025-04-11 15:11:45 +0800

    feature: use pest to parse json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let s = vec![1i32, 2i32];
+    let s = s.into_iter().next().ok_or(anyhow::anyhow!("labalaba"))?;
+    println!("{:?}", s);
+    Ok(())
 }
